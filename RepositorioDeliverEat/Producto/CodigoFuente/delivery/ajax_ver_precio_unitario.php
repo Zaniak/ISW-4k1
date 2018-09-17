933f1cbb2df87c6171762c8feccc7f2eb30182ca
Gastón Lozada <lozada.gaston@gmail.com>
2018-09-17 14:09:12 -0300

    se eliminan archivos innesesarios

--- RepositorioDeliverEat/Producto/CodigoFuente/delivery/ajax_ver_precio_unitario.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once('conexion.php');
-  conectar(); 
-  $sql = "SELECT * FROM comidas WHERE nombre_comida = '".$_REQUEST['comida']."'";
-  $result = mysql_query($sql);
-   	while($row = mysql_fetch_array($result)){
-   		echo $row['precio'];
-   	}
-?>
